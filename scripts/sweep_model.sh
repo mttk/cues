2991d0ca2e88158801b54fb619f1cafdfce102cf
Martin Tutek <martin.tutek@gmail.com>
2026-07-19 16:37:13 +0200

    Add script

--- scripts/sweep_model.sh
+++ scripts/sweep_model.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --partition=nlp      # partition
 #SBATCH --account=nlp      # account
-#SBATCH --gres=gpu:6000ADA:1        # Request 1 gpu type A40 > change to 2/1
+#SBATCH --gres=gpu:L40:1        # Request 1 gpu type A40 > change to 2/1
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1 # Change to 2/1
 #SBATCH --mem=5GB
